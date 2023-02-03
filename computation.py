d59190f1c9a51131631696e603eb3ec8e4222576
mcclurr <122703361+mcclurr@users.noreply.github.com>
2023-02-02 19:50:55 -0500

    Initializing Functions

---
+++ computation.py
@@ -0,0 +1,57 @@
+import numpy
+
+###################################### Inputs ######################################
+
+# gamma: Heat Coefficient Ratio
+# Me: Exit Mach Number
+# N: number of initial points
+# R: throat expansion curvature radius with respect to throat height
+
+###################################### Outputs #####################################
+
+# M: Mach Number Distribution
+
+######################### Variables Used for Generating the Shape ##################
+
+# delta: d
+# Prandtl-Meyer Angle: v
+# C- Characteristic Line: L
+# C+ Characteristic Line: R
+# Mach: M
+# Mach Angle: u
+# Slope of L Lines: Lslope
+# Slope of R Lines: Rslope
+# x
+# y
+
+##################################### Functions ###################################
+
+def nu(M, gamma):
+
+    return
+
+
+
+def nozzle(gamma, Me, n, R):
+
+# Initialize: d,
+
+
+    return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
+from computation import nozzle
+
+
+# Inputs
+Me = 2.286
+
+# Constants
+GAMMA = 1.4
+n = [8, 16]
+R = .5
 
 if __name__ == '__main__':
-    print('PyCharm')
+
+    area_ratio = []
+
+    for i in n:
+        area_ratio.append(nozzle(GAMMA, Me, n, R))
+
+
 
 
